a75ef83e8b2f10ed1a84aaa83fa5b75bf43f794e
bcfh <crunchy.wafer.stick787@gmail.com>
2023-08-09 15:33:17 +0800

    generate addresses for test

--- test/signer.spec.ts
+++ test/signer.spec.ts
@@ -1,12 +1,25 @@
+require('dotenv').config({ path: '/Users/bcfh/msigner/test/.env' });
 import * as bitcoin from 'bitcoinjs-lib';
 import * as chai from 'chai';
+import * as assert from 'assert';
+import ECPairFactory from 'ecpair';
+import * as ecc from 'tiny-secp256k1';
 import { IOrdItem, utxo } from '../src';
 import {
   ORDINALS_API_URL,
 } from '../src/constant';
 import { FullnodeRPC } from '../src/vendors/fullnoderpc';
+import {SellerSigner, BuyerSigner} from "../src/signer";
+import * as interfaces from "../src/interfaces";
 
 const expect = chai.expect;
+const ECPair = ECPairFactory(ecc);
+const TESTNET = bitcoin.networks.testnet;
+
+interface AddressInfo {
+  address: string;
+  privateKey: string;
+}
 
 const genIOrdItemFixture = (): IOrdItem => {
   return {
@@ -48,6 +61,7 @@ const getBuyerFixtureTx = async (): Promise<bitcoin.Transaction> => {
     outputIndex++
   ) {
     try {
+      // todo：为什么往这个outputIndex的output里面加入空的witness
       buyerFixtureTx.setWitness(outputIndex, []);
     } catch {}
   }
@@ -93,10 +107,89 @@ const genbuyerFixture = async () => {
   };
 };
 
+const generateTestnetAddress = async () : Promise<AddressInfo>  => {
+  // examples using other network information
+  const keyPair = ECPair.makeRandom({ network: TESTNET });
+  const { address } = bitcoin.payments.p2pkh({
+    pubkey: keyPair.publicKey,
+    network: TESTNET,
+  });
+
+  if (address) {
+    return {
+      address,
+      privateKey: keyPair.privateKey?.toString('hex') || '',
+    };
+  } else {
+    throw new Error("Error when creating .");
+  }
+}
+
+const generateTestnetSegwitAddress = async () : Promise<AddressInfo> => {
+  // examples using other network information
+  const keyPair = ECPair.makeRandom({ network: TESTNET });
+  const { address } = bitcoin.payments.p2wpkh({
+    pubkey: keyPair.publicKey,
+    network: TESTNET,
+  });
+
+  if (address) {
+    return {
+      address,
+      privateKey: keyPair.privateKey?.toString('hex') || '',
+    };
+  } else {
+    throw new Error("Error when creating .");
+  }
+}
+
+describe('Generate testnet addresses', async () => {
+  it('should generate a testnet legacy address', async () => {
+    const addressInfo = await generateTestnetAddress();
+    console.log("this is legacy addressInfo: ", addressInfo);
+    expect(addressInfo).to.not.be.undefined;
+  });
+  it('should generate a testnet segwit address', async () => {
+    const addressInfo = await generateTestnetSegwitAddress();
+    console.log("this is segwit addressInfo: ", addressInfo);
+    expect(addressInfo).to.not.be.undefined;
+  })
+});
+
+
 describe('SellerSigner', () => {
   describe('PSBT', () => {
     it('should generate an unsigned PSBT', async () => {
+      const fixture = await genbuyerFixture();
+      console.log("this is buyer fixture: ", fixture);
       true;
     });
   });
+
+
+  describe('generateUnsignedListingPSBTBase64', () => {
+    it('should generate unsigned listing PSBTBase64 and sign', async () => {
+      const ordItem = genIOrdItemFixture();
+
+      const listing: interfaces.IListingState = {
+        seller: {
+          makerFeeBp: 1,
+          sellerOrdAddress: "tb1p65gd97dj3eqz83n4ehder6pdq9pyv0alqd6nqthhassdca40wzssqaamkl",
+          price: 1,
+          ordItem: ordItem,
+          sellerReceiveAddress: "tb1p65gd97dj3eqz83n4ehder6pdq9pyv0alqd6nqthhassdca40wzssqaamkl",
+          unsignedListingPSBTBase64: "",
+          signedListingPSBTBase64: "",
+          tapInternalKey:""
+        },
+      };
+          
+      const unsignedPSBT = await SellerSigner.generateUnsignedListingPSBTBase64(listing);
+      console.log("this is unsignedPSBT: ", unsignedPSBT);
+
+    })
+  
+  })
+
 });
+
